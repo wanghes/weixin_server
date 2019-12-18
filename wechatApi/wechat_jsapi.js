426f82130112c26e33fb345334eca00a97756c08
haisong.wang <haisong.wang@9icaishi.net>
2019-12-18 17:25:14 +0800

    ad

--- wechatApi/wechat_jsapi.js
+++ wechatApi/wechat_jsapi.js
@@ -324,48 +324,18 @@ class Jsapi {
         var data = {
             'button':[
              {    
-                'type':'click',
-                'name':'最新',
-                'key':'V1001_TODAY_MUSIC'
+                'type':'view',
+                'name':'智慧巡店',
+                'key':'V1001_WISDOM_FIND_SHOP',
+                "url":"http://www.soso.com/"
              },
-             {
-                'name':'类别',
-                'sub_button':[
-                {    
-                     'type':'click',
-                  'name':'科幻',
-                  'key':'V1001_TYPE_KEHUAN'
-                },
-                {
-                  'type':'click',
-                  'name':'悬疑',
-                  'key':'V1001_TYPE_KXUANYI'
-                },
-                {
-                  'type':'click',
-                  'name':'爱情',
-                  'key':'V1001_TYPE_AIQING'
-                },
-                {
-                  'type':'click',
-                  'name':'教育',
-                  'key':'V1001_TYPE_JIAOYU'
-                }]
-                 },
-                 {
-                'name':'地域',
-                'sub_button':[
-                {    
-                     'type':'click',
-                  'name':'大陆',
-                  'key':'V1001_AREA_DALU'
-                },
-                {
-                  'type':'click',
-                  'name':'欧美',
-                  'key':'V1001_AREA_OUMEI'
-                }]
-            }]
+             {    
+                'type':'click',
+                'name':'帮你寻车',
+                'key':'V1001_HELP_YOU_FIND_CAR',
+                "url":"http://www.soso.com/"
+             }
+            ]
         };
 
         data = JSON.stringify(data);
