7891df1a77e8fec1494bd5757989b91af65e3062
haisong.wang <haisong.wang@9icaishi.net>
2020-01-13 17:51:54 +0800

    ad

--- util/encrypto.js
+++ util/encrypto.js
@@ -9,14 +9,14 @@ const encryptoSha1 = (pass) =>{
 
 // 创建对称加密 加密算法
 const aesEncode = (data, key = encryptoKey) => {
-    const cipher = crypto.createCipher('aes192', key);
+    const cipher = crypto.createCipheriv('aes192', key);
     var crypted = cipher.update(data, 'utf8', 'hex');
     crypted += cipher.final('hex');
     return crypted;
 }
 // 创建对称加密 解密算法
 const aesDecode = (encrypted, key = encryptoKey) => {
-    const decipher = crypto.createDecipher('aes192', key);
+    const decipher = crypto.createDecipheriv('aes192', key);
     var decrypted = decipher.update(encrypted, 'hex', 'utf8');
     decrypted += decipher.final('utf8');
     return decrypted;
